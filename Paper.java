c860361116f5ec415ee3c79b78b6482d13ded4ae
Mehereen-1 <ayesha.mehereen01@gmail.com>
2025-09-21 14:57:10 +0600

    added getAuthorName method into main

---
+++ Paper.java
@@ -0,0 +1,9 @@
+interface ResearchPaper {
+    String getAuthorName();
+}
+
+public class Paper{
+    public static void main(String[] args) {
+        System.out.println("Research Paper Interface");
+    }
+}
